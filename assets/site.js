7d2de85f35ec18d4be35aab6980811badadb6034
Greg Wilson <gvwilson@third-bit.com>
2017-09-02 09:27:24 -0400

    Switching to new layout

--- assets/site.js
+++
@@ -1,7 +0,0 @@
-// Handle searches.
-// Relies on document having 'meta' element with name 'search-domain'.
-function google_search() {
-  var query = document.getElementById("google-search").value;
-  var domain = $("meta[name=search-domain]").attr("value");
-  window.open("https://www.google.com/search?q=" + query + "+site:" + domain);
-}
