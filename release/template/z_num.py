86bc84aabfd1e62e011b30395dcaa91181b613fd
Greg Wilson <gvwilson@third-bit.com>
2023-09-05 06:31:44 -0400

    Removing unused files

--- release/template/z_num.py
+++
@@ -1,8 +0,0 @@
-"""Insert a constant."""
-
-def open(expander, node):
-    expander.showTag(node, False)
-    expander.output(node.attrs["z-num"])
-
-def close(expander, node):
-    expander.showTag(node, True)
