fa95e561b024101fc4739957867c8e86e5e6cdc3
Greg Wilson <gvwilson@third-bit.com>
2025-02-26 21:10:44 -0500

    another student

--- roster/roster.sql
+++ roster/roster.sql
@@ -625,6 +625,7 @@ INSERT INTO person VALUES('lawson.lance', 'Lawson', 'Lance', 'M', 215, 'lance.b.
 INSERT INTO person VALUES('dursi.jonatan', 'Dursi', 'Jonathan', 'M', 216, 'ljdursi@scinet.utoronto.ca');
 INSERT INTO person VALUES('susanto.daelenia', 'Susanto', 'Daelenia', 'F', 78, 'daelenia98@gmail.com');
 INSERT INTO person VALUES('seong.rachel', 'Seong', 'Rachel', 'F', 85, 'sungeunchae@gmail.com');
+INSERT INTO person VALUES('wang.evence', 'Wang', 'Evence', 'M', 7, 'shjp20070426@gmail.com');
 
 CREATE TABLE country(
        iso text not null,
